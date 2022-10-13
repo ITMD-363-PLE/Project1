0bcb9a7b6eae455dcd6714954a63a7d0bf1fb89a
Philip Le <lephil505006@gmail.com>
2022-10-13 18:29:08 -0500

    Adding the javascript portion

---
+++ js/site.js
@@ -0,0 +1,21 @@
+function validateForm() {
+    let x = document.forms["myForm"]["name"].value;
+    let y = document.forms["myForm"]["lname"].value;
+    let z = document.forms["myForm"]["email"].value;
+    if (x == "" && y == "" && z == "") {
+        alert("All fields must be filled out");
+        return false;
+    }
+    else if (x == "") {
+        alert("First name field must be filled out");
+        return false;
+    }
+    else if (y == "") {
+        alert("Last name field must be filled out");
+        return false;
+    }
+    else if (z == "") {
+        alert("Email field must be filled out");
+        return false;
+    }
+}
